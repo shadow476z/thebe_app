773e6d8e931cf796e2d30b30b09f278ba131c9f9
shadow476 <shadow476z@gmail.com>
2013-12-02 09:08:40 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThebeApp::Application.config.secret_key_base = '662835de480b979204ce827c5b30fb587ac93c40b4e9556e0ed9c1f82db1b0034645b242aa24b93af8fda76f6ff354f884f46b5d22f0a14ecaf0bd4a1f4e3080'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThebeApp::Application.config.session_store :cookie_store, key: '_thebe_app_session'
